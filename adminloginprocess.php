df54deda1931b811d2fcc7cfad1167342940f2ac
blob35 <135.subah.pvt@gmail.com>
2025-01-03 17:28:16 +0600

    Update adminloginprocess.php

--- adminloginprocess.php
+++ adminloginprocess.php
@@ -1,18 +1,20 @@
 <?php
 require_once('connect.php');
 
-if(isset($_POST['email']) && isset($_POST['password'])){
+if (!empty($_POST['username']) && !empty($_POST['password'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
+
     $sql = "SELECT * FROM admin WHERE Username = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($result) > 0){
-        
-        header("Location: homepage.php");
+    if (mysqli_num_rows($result) > 0) {
+        header("Location: admin_dashboard.php");
+        exit();
+    } else {
+        echo "<script>alert('Wrong username or password. Please try again.'); window.location.href='adminlogin.php';</script>";
     }
-    else{
-        echo "Wrong username or password";
-    }
+} else {
+    echo "<script>alert('Username and password cannot be empty.'); window.location.href='adminlogin.php';</script>";
 }
 ?>

c5ebe2402c7f6685adb7653167e36e69a6022286
blob35 <135.subah.pvt@gmail.com>
2025-01-04 08:28:32 +0600

    Update login.php

--- login.php
+++ login.php
@@ -1,31 +1,53 @@
+<?php
+session_start();
+
+require_once "connect.php";
+
+if (isset($_POST["login"])) {
+    // Retrieve form data
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+
+    // Query to check if email exists
+    $sql = "SELECT * FROM customer WHERE email = '$email'";
+    $result = mysqli_query($conn, $sql);
+    $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+    if ($user) {
+        header("Location: userprofile.php");
+          exit();
+        } else {
+            echo "<div class='alert alert-danger'>Email does not match</div>";
+        }
+    }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Form </title>
+  <title>Login Form</title>
   <link rel="stylesheet" href="style.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 <body>
   <div class="wrapper">
-    <form action="loginprocess.php">
+    <form action="login.php" method="POST">
       <h1>Login</h1>
       <div class="input-box">
-        <input type="text" placeholder="Email" required>
-        <i class='bx bxs-user'></i>
+        <input type="email" name="email" placeholder="Enter Email" required>
+        <i class='bx bxs-envelope'></i>
       </div>
       <div class="input-box">
-        <input type="password" placeholder="Password" required>
-        <i class='bx bxs-lock-alt' ></i>
+        <input type="password" name="password" placeholder="Enter Password" required>
+        <i class='bx bxs-lock-alt'></i>
       </div>
-
-      <a href="selectdate.php" class="login">Login</a>
-         <br>
-         <p>Don't have an account? <a href="registration.php" style="color: white;">Register</a></p>
-      </div>
+      <button type="submit" name="login" class="login">Login</button>
     </form>
+    <div class="register-link">
+      <p>Not registered yet? <a href="registration.php">Register Here</a></p>
+    </div>
   </div>
 </body>
 </html>

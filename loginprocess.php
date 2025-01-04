08800e5ce1076ccdafa02a5df6c0e3fa99e5ca61
Lusaka2003 <141989127+Lusaka2003@users.noreply.github.com>
2025-01-04 13:39:39 +0600

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -1,20 +1,21 @@
-// <?php
-// require_once('connect.php');
+<?php
+require_once('connect.php');
+if (!empty($_POST['email']) && !empty($_POST['Password'])) {
+    $email = $_POST['email'];
+    $Password = $_POST['Password'];
+    
+    $sql = "SELECT * FROM customer WHERE email = '$email' AND Password = '$Password'";
+    $result = mysqli_query($conn, $sql);
 
-// if(isset($_POST['email']) && isset($_POST['password'])){
-//     $email = $_POST['email'];
-//     $password = $_POST['password'];
-//     $sql = "SELECT * FROM customers WHERE email = '$email' AND password = '$password'";
-//     $result = mysqli_query($conn, $sql);
-
-//     if(mysqli_num_rows($result) > 0){
+    if (mysqli_num_rows($result) > 0) {
         
-//         header("Location: homepage.php");
-//     }
-//     else{
-//         echo "Wrong username or password";
-//     }
-// }
-// ?>
+        header("Location: userprofile.php");
+        exit();
 
-// not needed 
+    } else {
+        echo "<script>alert('Wrong username or password. Please try again.'); window.location.href='login.php';</script>";
+    }
+} else {
+    echo "<script>alert('Username and password cannot be empty.'); window.location.href='login.php';</script>";
+}
+?>

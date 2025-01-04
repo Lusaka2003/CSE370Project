0cec8b8cbb4aa79c2f95cbe901a08e9991f1eef3
blob35 <135.subah.pvt@gmail.com>
2025-01-04 08:29:17 +0600

    Update loginprocess.php

--- loginprocess.php
+++ loginprocess.php
@@ -1,18 +1,20 @@
-<?php
-require_once('connect.php');
+// <?php
+// require_once('connect.php');
 
-if(isset($_POST['email']) && isset($_POST['password'])){
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    $sql = "SELECT * FROM customers WHERE email = '$email' AND password = '$password'";
-    $result = mysqli_query($conn, $sql);
+// if(isset($_POST['email']) && isset($_POST['password'])){
+//     $email = $_POST['email'];
+//     $password = $_POST['password'];
+//     $sql = "SELECT * FROM customers WHERE email = '$email' AND password = '$password'";
+//     $result = mysqli_query($conn, $sql);
 
-    if(mysqli_num_rows($result) > 0){
+//     if(mysqli_num_rows($result) > 0){
         
-        header("Location: homepage.php");
-    }
-    else{
-        echo "Wrong username or password";
-    }
-}
-?>
+//         header("Location: homepage.php");
+//     }
+//     else{
+//         echo "Wrong username or password";
+//     }
+// }
+// ?>
+
+// not needed 

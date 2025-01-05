d4d9d35b865a10ea296f9afd1edcd029dad65426
Lusaka2003 <141989127+Lusaka2003@users.noreply.github.com>
2025-01-05 11:47:38 +0600

    Update addoffer.php

--- addoffer.php
+++ addoffer.php
@@ -2,36 +2,38 @@
 include "connect.php";  // Include your database connection file
 
 // Declare variables
-$Promo_Code = $Description = $Promo_Type = $Percentage = $Discounted_Amount = "";
-$Promo_CodeErr = $DescriptionErr = $Promo_TypeErr = $PercentageErr = $Discounted_AmountErr = "";
+$Promo_Code=$Description=$Percentage="";
+$Promo_CodeErr=$DescriptionErr=$PercentageErr="";
 
 // If the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Capture form data
     $Promo_Code = $_POST['Promo_Code'];
     $Description = $_POST['Description'];
-    $Promo_Type = $_POST['Promo_Type'];
     $Percentage = $_POST['Percentage'];
-    $Discounted_Amount = $_POST['Discounted_Amount'];
+
 
     // Validate form fields
     if (empty($Promo_Code)) {
         $Promo_CodeErr = "Promo Code is required.";
     }
-    if (empty($Promo_Type)) {
-        $Promo_TypeErr = "Promo Type is required.";
+    if (empty($Description)) {
+        $DescriptionErr = "Description is required.";
+    }
+    if (empty($Percentage)) {
+        $PercentageErr = "Percentage is required.";
     }
 
     // If all fields are valid
-    if (empty($Promo_CodeErr) && empty($Promo_TypeErr)) {
+    if (empty($Promo_CodeErr) && empty($DescriptionErr)) {
         // Prepare the SQL INSERT query
-        $query = "INSERT INTO offer_details (Promo_Code, Description, Promo_Type, Percentage, Discounted_Amount) 
-                  VALUES (?, ?, ?, ?, ?)";
+        $query = "INSERT INTO offer_details (Promo_Code,Description,Percentage) 
+                  VALUES (?, ?, ?)";
 
         // Use prepared statements to prevent SQL injection
         if ($stmt = mysqli_prepare($conn, $query)) {
             // Bind the parameters to the prepared statement
-            mysqli_stmt_bind_param($stmt, "sssss", $Promo_Code, $Description, $Promo_Type, $Percentage, $Discounted_Amount);
+            mysqli_stmt_bind_param($stmt, "sss", $Promo_Code,$Description, $Percentage);
 
             // Execute the query
             if (mysqli_stmt_execute($stmt)) {
@@ -73,23 +75,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <input type="text" placeholder="Enter Promo Code" name="Promo_Code" value="<?php echo htmlspecialchars($Promo_Code); ?>" required>
             <span class="error"><?php echo $Promo_CodeErr; ?></span><br>
 
-            <!-- Description -->
             <label for="Description"><b>Description</b></label><br>
-            <input type="text" placeholder="Enter Offer Description" name="Description" value="<?php echo htmlspecialchars($Description); ?>"><br>
+            <input type="text" placeholder="Description" name="Description" value="<?php echo $Description; ?>">
+            <span class="error"><?php echo $DescriptionErr; ?></span><br>
 
-            <!-- Promo Type -->
-            <label for="Promo_Type"><b>Promo Type</b></label><br>
-            <input type="text" placeholder="Enter Promo Type" name="Promo_Type" value="<?php echo htmlspecialchars($Promo_Type); ?>" required>
-            <span class="error"><?php echo $Promo_TypeErr; ?></span><br>
 
-            <!-- Percentage -->
             <label for="Percentage"><b>Percentage</b></label><br>
             <input type="text" placeholder="Percentage" name="Percentage" value="<?php echo $Percentage; ?>">
             <span class="error"><?php echo $PercentageErr; ?></span><br>
-    
-            <label for="Discounted_Amount"><b>Discounted_Amount</b></label><br>
-            <input type="text" placeholder="Discounted_Amount" name="Discounted_Amount" value="<?php echo $Discounted_Amount; ?>">
-            <span class="error"><?php echo $Discounted_AmountErr; ?></span><br>
 
             <br>
             <button type="submit" class="registerbtn">Add Offer</button>
